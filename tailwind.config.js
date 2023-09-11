321c4508f45c0c8f860f67e2a8ef247c300c3dfc
Matthew <107056920+s023322@users.noreply.github.com>
2023-09-11 18:34:47 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./pages/*.html", "./src/*.js"],
+  theme: {
+    fontFamily: {
+      'dm': ['"DM"'],
+      'icon': ['Icons'],
+    },
+    colors: {
+      'dm-text': '#F6E0DF',
+      'lm-text': '#0E0004',
+      'dm-primary': '#090C0A',
+      'lm-primary': '#9CADCE',
+      'dm-secondary': '#001929',
+      'lm-secondary': '#D1CFE2',
+      'main-blue': '#334195',
+      'main-red': '#A5243D',
+    },
+  },
+  plugins: [],
+}
+
