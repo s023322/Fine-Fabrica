9dff4740866716b146d67f3fb956395d747b5269
Matthew <107056920+s023322@users.noreply.github.com>
2023-10-28 12:01:25 -0400

    Add files via upload

--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,23 @@
 if (localStorage.theme == 'dark') {
-    localStorage.theme = 'dark'
-    document.documentElement.classList.add('dark')
-    document.getElementById('mode').innerHTML = "light_mode"
+    localStorage.theme = 'dark';
+    document.documentElement.classList.add('dark');
+    document.getElementById('theme_change').innerHTML = "light_mode";
 }
 else {
-    localStorage.theme = 'light'
-    document.documentElement.classList.remove('dark')
-    document.getElementById('mode').innerHTML = "dark_mode"
+    localStorage.theme = 'light';
+    document.documentElement.classList.remove('dark');
+    document.getElementById('theme_change').innerHTML = "dark_mode";
 }
 
-
 function mode() {
-    if (localStorage.theme == 'light') {
-        localStorage.theme = 'dark'
-        document.documentElement.classList.add('dark')
-        document.getElementById('mode').innerHTML = "light_mode"
-        console.log(localStorage.theme)
+    if (localStorage.theme == 'light' || !document.documentElement.classList.contains("dark")) {
+        localStorage.theme = 'dark';
+        document.documentElement.classList.add('dark');
+        document.getElementById('theme_change').innerHTML = "light_mode";
     }
     else {
-        localStorage.theme = 'light'
-        document.documentElement.classList.remove('dark')
-        document.getElementById('mode').innerHTML = "dark_mode"
-        console.log(localStorage.theme)
+        localStorage.theme = 'light';
+        document.documentElement.classList.remove('dark');
+        document.getElementById('theme_change').innerHTML = "dark_mode";
     }
 }
