521349d77f9a374f1438bbb3669731c02ba428dd
Matthew <107056920+s023322@users.noreply.github.com>
2024-02-25 10:44:06 -0500

    Add files via upload

---
+++ src/stripe.js
@@ -0,0 +1,45 @@
+var stripe = Stripe(
+  "pk_test_51OnOAzH4ceWkCZ13BXbBaxpkT8RSdA3CgH9yczcnwK5hCAXqm4o551aVupJla4LVXNRP8DlqSTMMYhMXNM7ujPyN00sR6BTImW"
+);
+
+document.getElementById("tier1").addEventListener("click", function () {
+  stripe.redirectToCheckout({
+    lineItems: [
+      {
+        price: "price_1OnOgbH4ceWkCZ13pUtWKkjD",
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    successUrl: "http://localhost:5500/pages/membership.html",
+    cancelUrl: "http://localhost:5500/pages/services.html",
+  });
+});
+
+document.getElementById("tier2").addEventListener("click", function () {
+  stripe.redirectToCheckout({
+    lineItems: [
+      {
+        price: "price_1OnOffH4ceWkCZ13GvIbBRhX",
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    successUrl: "http://localhost:5500/pages/membership.html",
+    cancelUrl: "http://localhost:5500/pages/services.html",
+  });
+});
+
+document.getElementById("tier3").addEventListener("click", function () {
+  stripe.redirectToCheckout({
+    lineItems: [
+      {
+        price: "price_1OnOfqH4ceWkCZ13ZjKqKgy4",
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    successUrl: "http://localhost:5500/pages/membership.html",
+    cancelUrl: "http://localhost:5500/pages/services.html",
+  });
+});
